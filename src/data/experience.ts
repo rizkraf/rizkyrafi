070a33bf0030c8c540a184b3148ee538ac24b6b9
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-08-17 17:18:12 +0700

    refactor: :sparkles: Update experience.ts and about.astro file
    Refactor the experience.ts file to provide more detailed descriptions of past roles. Update the about.astro page to provide a comprehensive overview of my background and experience in frontend development.

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,7 +9,7 @@ const PAST_ROLES: {
     company: 'Roketin',
     role: 'Frontend Engineer',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
+      'I worked on the front end of one of the reinsurance company projects. I was responsible for the front-end development of the project. I used Vue.js and Nuxt.js for the first project and React, Redux Toolkit, and Typescript for the second project.',
     startDate: 'September 2023',
     endDate: 'Present',
   },
