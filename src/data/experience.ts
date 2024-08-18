0ba4b16a3611bcffd808d86ed7623dcbb9a48aed
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-08-18 13:26:12 +0700

    refactor: :speech_balloon: Update experience and project descriptions

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -9,7 +9,7 @@ const PAST_ROLES: {
     company: 'Roketin',
     role: 'Frontend Engineer',
     description:
-      'I worked on the front end of one of the reinsurance company projects. I was responsible for the front-end development of the project. I used Vue.js and Nuxt.js for the first project and React, Redux Toolkit, and Typescript for the second project.',
+      'I worked on the front end of one of the reinsurance company projects. I was responsible for the front-end development of the project. Overall, i used Typescript, Vue.js, Nuxt.js, React, Redux Toolkit, and Tailwind CSS as my tech stack.',
     startDate: 'September 2023',
     endDate: 'Present',
   },
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,7 +7,8 @@ const PROJECTS: {
 }[] = [
   {
     name: 'Quran Web',
-    description: 'Lorem ipsum dolor sit amet',
+    description:
+      'A simple web app to read the Quran. You can read, listen, search, and bookmark the verses in the Quran. This app is built using Vue.js, Pinia, Tailwind CSS, and the Quran API.',
     liveLink: 'https://quran.rizkyrafi.id/',
     previewImage:
       'https://seryrzlikbdsaklwhqhw.supabase.co/storage/v1/object/sign/rizkyraf-storages/Quran-Web.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyaXpreXJhZi1zdG9yYWdlcy9RdXJhbi1XZWIucG5nIiwiaWF0IjoxNzIzODkwMzA1LCJleHAiOjE3NTU0MjYzMDV9._JhIU1N0TWwg2DwHe8rdr4OXzRldXBYQhEWrdbWs6X8&t=2024-08-17T10%3A25%3A07.147Z',
@@ -15,7 +16,8 @@ const PROJECTS: {
   },
   {
     name: 'Noter',
-    description: 'Lorem ipsum dolor sit amet',
+    description:
+      'A simple note-taking app. You can create, read, update, and delete notes. This app is built using React and Tailwind CSS.',
     liveLink: 'https://noter.rizkyrafi.id/',
     previewImage:
       'https://seryrzlikbdsaklwhqhw.supabase.co/storage/v1/object/sign/rizkyraf-storages/Noter.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyaXpreXJhZi1zdG9yYWdlcy9Ob3Rlci5wbmciLCJpYXQiOjE3MjM4OTAzMTgsImV4cCI6MTc1NTQyNjMxOH0._xGihvsiiuaXkT4Jm1A2_Yygo9_6O71QEhiUij5b2r8&t=2024-08-17T10%3A25%3A19.475Z',
@@ -23,7 +25,8 @@ const PROJECTS: {
   },
   {
     name: 'Bookshelf',
-    description: 'Lorem ipsum dolor sit amet',
+    description:
+      'A simple bookshelf app. You can add, read, update, and delete books. This app is built using Vanilla JavaScript',
     liveLink: 'https://bookshelf-website.netlify.app/',
     previewImage:
       'https://seryrzlikbdsaklwhqhw.supabase.co/storage/v1/object/sign/rizkyraf-storages/Bookshelf.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyaXpreXJhZi1zdG9yYWdlcy9Cb29rc2hlbGYucG5nIiwiaWF0IjoxNzIzODkwMTgzLCJleHAiOjE3NTU0MjYxODN9.zPTsGO-vqyh2MexZNsJ1tHOzrGYMtR-0k7lt7hGepFo&t=2024-08-17T10%3A23%3A04.950Z',
@@ -31,7 +34,8 @@ const PROJECTS: {
   },
   {
     name: 'Spense',
-    description: 'Lorem ipsum dolor sit amet',
+    description:
+      'A landing page from Frontend Mentor challenge. This app is built using HTML, CSS, and JavaScript.',
     liveLink: 'https://spense-web.netlify.app/',
     previewImage:
       'https://seryrzlikbdsaklwhqhw.supabase.co/storage/v1/object/sign/rizkyraf-storages/Spense.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyaXpreXJhZi1zdG9yYWdlcy9TcGVuc2UucG5nIiwiaWF0IjoxNzIzODkwMzI5LCJleHAiOjE3NTU0MjYzMjl9.oMksBDT2SrfUGYW4S0tN1mz67bXuHlRjn3c9qaW7DeU&t=2024-08-17T10%3A25%3A30.959Z',
@@ -39,7 +43,7 @@ const PROJECTS: {
   },
   {
     name: 'Pong Game',
-    description: 'Lorem ipsum dolor sit amet',
+    description: 'A simple pong game. This app is built using Unity and C#.',
     liveLink: '/',
     previewImage:
       'https://seryrzlikbdsaklwhqhw.supabase.co/storage/v1/object/sign/rizkyraf-storages/Pong.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyaXpreXJhZi1zdG9yYWdlcy9Qb25nLnBuZyIsImlhdCI6MTcyMzg5MDM0MSwiZXhwIjoxNzU1NDI2MzQxfQ.bJ1Kb_ctHE0ndwMhNWrg3WYZsCiESfbAfo9tOKrJPIU&t=2024-08-17T10%3A25%3A43.246Z',
